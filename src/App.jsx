f6cfe9b18e4ed63f716d8b71bb9614d7b61c5507
Siu <siuc@protonmail.com>
2022-05-21 00:08:02 -0400

    add spin when loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
 import "App.css";
 import { StateContext } from "StateContext";
+import Spin from "components/Spin";
 
 function App() {
   const { state } = useContext(StateContext);
 
-  return <>hello{state.loading && "LOADING"}</>;
+  return <>{state.loading && <Spin />}</>;
 }
 
 export default App;
---
+++ src/components/Spin/index.jsx
@@ -0,0 +1,7 @@
+import "./Spin.css";
+
+const Spin = () => {
+  return <div className="spin" />;
+};
+
+export default Spin;
