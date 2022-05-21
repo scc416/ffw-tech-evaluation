3964f0ef7d1509eb88c185fe1a4357031f86de9d
Siu <siuc@protonmail.com>
2022-05-21 00:29:44 -0400

    new file for error

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const initialState = { error: null, tabs: null };
+export const initialState = { error: null, tabs: null, tabId: 0, fontId: null };
 
 export const SHOW_ERROR = "SHOW_ERROR";
 export const FETCHED_TABS = "FETCHED_TABS";
