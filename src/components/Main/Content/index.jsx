e838b53c8b24c680cada0a919a1ab1f7719efc67
Siu <siuc@protonmail.com>
2022-05-21 01:25:38 -0400

    display id of the fonts

--- src/components/Main/Content/index.jsx
+++ src/components/Main/Content/index.jsx
@@ -1,12 +1,21 @@
 import { useContext } from "react";
 import { StateContext } from "StateContext";
+import { checkIfIsFonts } from "helpers";
 
 const Content = () => {
   const {
-    state: { tabs, tabId }
+    state: { tabs, tabId },
   } = useContext(StateContext);
 
-  return <div></div>;
+  const details = tabs[tabId];
+  const isFonts = checkIfIsFonts(details);
+  const { content } = details;
+  return (
+    <div>
+      {isFonts && content.map((x) => <div key={x.id}>{x.id}</div>)}
+      {!isFonts && content}
+    </div>
+  );
 };
 
 export default Content;
